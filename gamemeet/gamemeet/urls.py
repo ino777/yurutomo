0ddfbd53c958011897dd5752246601dd90d87de0
Manato Inoue <tarou.xxx.0129@gmail.com>
2021-04-02 20:40:40 +0900

    urls.pyにdjango.contrib.auth.urlsを追加

--- gamemeet/gamemeet/urls.py
+++ gamemeet/gamemeet/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chatrooms.urls')),
     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
 
 if settings.DEBUG:
