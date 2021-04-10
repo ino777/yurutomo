22719ab0a2dd8eabeef0e18c67f5dd5178d26907
Manato Inoue <tarou.xxx.0129@gmail.com>
2021-04-11 03:10:35 +0900

    モデル調整

--- gamemeet/chatrooms/models.py
+++ gamemeet/chatrooms/models.py
@@ -11,10 +11,38 @@ User = get_user_model()
 
 class Tag(models.Model):
     name = models.CharField(_('tag name'), max_length=255)
+    
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+    
+    def clean(self):
+        """
+        name から空白をなくす
+        """
+        if self.name:
+            self.name = "".join(self.name.split())
 
 class Topic(models.Model):
     name = models.CharField(_('topic name'), max_length=255)
     tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+    
+    def clean(self):
+        """
+        name から空白をなくす
+        """
+        if self.name:
+            self.name = "".join(self.name.split())
 
 class Room(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
