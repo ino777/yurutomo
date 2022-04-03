8d81bc9100fc4f46a8e912b2ccd2cbfc833174aa
Manato Inoue <tarou.xxx.0129@gmail.com>
2022-04-03 19:50:52 +0900

    DBをPostgreSQLに変更

--- yurutomo/yurutomo/settings.py
+++ yurutomo/yurutomo/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = 'gamemeet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DBNAME'],
+        'HOST': os.environ['DBHOST'],
+        'USER': os.environ['DBUSER'],
+        'PASSWORD': os.environ['DBPASS'] 
     }
 }
 
