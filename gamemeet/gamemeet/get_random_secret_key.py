480d187e3d5e18a9cadd376894b3f6658fb76abe
Manato Inoue <tarou.xxx.0129@gmail.com>
2022-04-03 17:48:56 +0900

    get_random_secret_keyを修正

--- gamemeet/gamemeet/get_random_secret_key.py
+++ gamemeet/gamemeet/get_random_secret_key.py
@@ -1,5 +1,11 @@
-from django.core.management.utils import get_random_secret_key
+if __name__ == '__main__':
+    from pathlib import Path
+    from django.core.management.utils import get_random_secret_key
 
-secret_key = get_random_secret_key()
-text = 'SECRET_KEY = \'{}\''.format(secret_key)
-print(text)
+    secret_key = get_random_secret_key()
+    text = 'SECRET_KEY = \'{}\''.format(secret_key)
+
+    parent_path = Path(__file__).resolve().parent
+    filepath = parent_path / 'local_settings.py'
+    with open(filepath, 'w+', encoding='utf-8') as f:
+        f.write(text)
